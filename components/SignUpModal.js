5e230dbdc71d8729a656558a365a0a4b8f9e8036
Your name <Your email>
2025-05-02 15:08:53 +0200

    branch sign

--- components/SignUpModal.js
+++ components/SignUpModal.js
@@ -2,8 +2,33 @@
 import React from 'react';
 import { Modal, Input, Button } from 'antd';
 import styles from '../styles/SignUpModal.module.css';
+// import {useState} from 'react'
+// import { useDispatch, useSelector } from 'react-redux';
+// import { login, logout } from '../reducers/user';
+
+// const [signUpUsername, setSignUpUsername] = useState('');
+// const [signUpPassword, setSignUpPassword] = useState('');
+// const [signUpKickername, setSignUpKickername] = useState('');
+
+
 
 const SignUpModal = ({ visible, onCancel }) => {
+  const handleSignUp = () => {
+    fetch('http://localhost:3000/users/kickup', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ firstname: signUpUsername, password: signUpPassword, username: signUpKickername }),
+    }).then(response => response.json())
+      .then(data => {
+        if (data.result) {
+          // dispatch(login({ firstname: signUpUsername, token: data.token }));
+          // setSignUpUsername('');
+          // setSignUpPassword('');
+          // setSignUpKickername('');
+        }
+      });
+  };
+  
   return (
     <Modal
       open={visible}
@@ -19,12 +44,13 @@ const SignUpModal = ({ visible, onCancel }) => {
         <Input className={styles.input} placeholder="Premiername" />
         <Input placeholder="Kickername" className={styles.input} />
         <Input.Password placeholder="JCVDMDP" className={styles.input} />
-        <Button type="primary" className={styles.signupButton}>
+        <Button type="primary" className={styles.signupButton} onClick={() => handleSignUp()}>
           Kick up
         </Button>
       </div>
     </Modal>
   );
 };
+
 
 export default SignUpModal;
